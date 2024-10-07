7c48958cdcd34e18057dd5910190a8f8f91dc26b
Skyggebjerg <ole@skyggebjerg.dk>
2024-10-07 15:49:15 +0200

    working

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,6 @@
 */
 
 #include <Arduino.h>
-//#include <M5Stack.h>
 #include "M5AtomS3.h"
 #include <M5GFX.h>
 #include "Unit_Encoder.h"
@@ -15,17 +14,16 @@ M5Canvas canvas(&display);
 Unit_Encoder sensor;
 
 void setup() {
+    Serial.begin(115200);
     Wire.begin(2,1);
     auto cfg = M5.config();
     AtomS3.begin(cfg);
 
-
-    //M5.begin(true, false, true, true);  // Init M5Stack.  初始化M5Stack
     sensor.begin();
-    display.begin();
-    display.setRotation(1);
-    canvas.setTextSize(2);
-    canvas.createSprite(160, 80);
+    AtomS3.Display.setTextColor(GREEN);
+    AtomS3.Display.setTextSize(2);
+    AtomS3.Display.drawString("PaHUB Example", AtomS3.Display.width() / 2, AtomS3.Display.height() / 2);
+
 }
 
 signed short int last_value = 0;
@@ -47,9 +45,11 @@ void loop() {
     if (!btn_stauts) {
         sensor.setLEDColor(0, 0xC800FF);
     }
-    canvas.fillSprite(BLACK);
-    canvas.drawString("BTN:" + String(btn_stauts), 10, 10);
-    canvas.drawString("ENCODER:" + String(encoder_value), 10, 40);
-    canvas.pushSprite(0, 0);
+    
+    AtomS3.Display.clear();
+    AtomS3.Display.drawString("C: ", 5, 5);
+    AtomS3.Display.drawString(String(btn_stauts), 40, 5);
+    AtomS3.Display.drawString(String(encoder_value), 40, 20);
+
     delay(20);
 }
